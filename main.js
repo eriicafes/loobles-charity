bb3f298c939b7e7f20f220f125421ba87646e807
tonayam <tonayam99@gmail.com>
2023-03-15 12:28:21 +0100

    First Commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// // show/hide
+
+// const menu = document.querySelector(".nav_menu");
+// const menuBtn = document.querySelector("#open-menu-btn");
+// const closeBtn = document.querySelector("#close-menu-btn");
+
+
+// menuBtn.addEventlistener('click', () => {
+//     menu.style.display = "flex";
+//     closeBtn.style.display = "inline-block";
+//     menuBtn.style.display = "none";
+// })
